accb6e139b073891ea7daf7dd711d6e5d4ebb0c5
Jorge Arce <jorgearcedev1@gmail.com>
2026-05-27 14:57:22 -0700

    Creando Layout Principal

---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import IndexPage from './views/IndexPage'
+import FavoritesPage from './views/FavoritesPage'
+import Layout from './layouts/Layout'
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<IndexPage />} index/>
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+
+    
